a73e15b94a1d32380af0e37a0f804e60f3825f21
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-07-10 11:00:52 -0300

    feat: update home

--- aulas/aula207_ola_django/home/views.py
+++ aulas/aula207_ola_django/home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    print('home')
+    
+    context = {
+    'text':'Olá home'
+    }
+
+    return render(
+        request,
+        'home/index.html',
+        context,
+    )
